99f72344d712f6dcbf696a3bcd92545a388c3e52
Tyrael <xykingofskillyx@gmail.com>
2023-09-15 20:37:38 +0200

    Rework of Result.java

--- src/main/java/de/aethos/lib/util/Result.java
+++ src/main/java/de/aethos/lib/util/Result.java
@@ -1,157 +1,80 @@
 package de.aethos.lib.util;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Optional;
-import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
 
 
-public class Result<V, R> {
-    private final V value;
+public class Result<V, E> {
+    private final V ok;
+    private final E error;
+    private final State state;
 
-    private final R reason;
-
-    private final Exception exception;
-
-    private final ResultStatus status;
-
-
-    private Result(V value, R reason, Exception exception, ResultStatus status) {
-        this.value = value;
-        this.reason = reason;
-        this.exception = exception;
-        this.status = status;
+    private Result(V ok, E error, State state) {
+        this.ok = ok;
+        this.error = error;
+        this.state = state;
     }
 
-    public static <V, R> Result<V, R> of(Optional<V> optional) {
-        try {
-            return Result.ok(optional.get());
-        } catch (Exception e) {
-            return Result.error(e);
-        }
+    public static <V,E> Result<V,E> ok(V v){
+        return new Result<>(v,null,State.OK);
     }
 
-    public static <V, R> Result<V, R> of(Callable<Result<V, R>> callable) {
-        try {
-            return callable.call();
-        } catch (Exception e) {
-            return Result.error(e);
-        }
+    public static <V,E> Result<V,E> error(E e){
+        return new Result<>(null,e,State.ERROR);
     }
 
-    public static <V, R> Result<V, R> ok(V value) {
-        if (value != null) {
-            return new Result<>(value, null, null, ResultStatus.VALUE);
-        }
-        return Result.error(new NullPointerException());
-    }
-
-    public static <V, R> Result<V, R> reason(R reason) {
-        return new Result<>(null, reason, null, ResultStatus.REASON);
-    }
-
-    public static <V, R> Result<V, R> error(Exception reason) {
-        return new Result<>(null, null, reason, ResultStatus.ERROR);
-    }
-
-    public boolean isOk() {
-        return value != null && !hasReason();
-    }
-
-
-    public void ifPresent(Consumer<V> action) {
-        if (value != null) {
-            action.accept(value);
-        }
-    }
-
-    public void ifPresentOrElse(Consumer<V> valueCon, Consumer<Exception> reasonCon) {
-        if (isOk()) {
-            valueCon.accept(value);
+    public static <V> Result<V,NullPointerException> fromOpt(Optional<V> opt){
+        if (opt.isPresent()){
+            return Result.ok(opt.get());
         } else {
-            reasonCon.accept(exception);
-        }
-    }
-
-
-    public @NotNull V getOrElseThrow() {
-        if (value == null) {
-            if (exception instanceof RuntimeException exception) {
-                throw exception;
-            } else {
-                throw new RuntimeException(exception);
-            }
-        } else {
-            return value;
-        }
-    }
-
-    public @NotNull V getOrDefault(@NotNull V def) {
-        if (value == null) {
-            return def;
-        } else {
-            return value;
-        }
-    }
-
-
-    public Optional<V> asOptional() {
-        return Optional.of(value);
-    }
-
-
-    public Result<V, R> filter(Predicate<? super V> predicate) {
-        if (predicate == null) {
             return Result.error(new NullPointerException());
         }
-        try {
-            if (isOk() && predicate.test(value)) {
-                return this;
-            } else {
-                return Result.error(new NullPointerException("Value not present"));
-            }
-        } catch (Exception e) {
-            return Result.error(e);
+    }
+
+    public Optional<V> getValue(){
+        if (state.equals(State.OK)){
+            return Optional.of(this.ok);
         }
+        return Optional.empty();
     }
-
-    public <T> T map(Function<V, T> methode) {
-        return methode.apply(value);
-    }
-
-    public boolean hasReason() {
-        return exception != null;
-    }
-
-    public R getReasonOrElseThrow() {
-        if (reason != null) {
-            return reason;
+    public Optional<E> getError(){
+        if (state.equals(State.ERROR)){
+            return Optional.of(error);
         }
-        throw new RuntimeException(exception);
+        return Optional.empty();
     }
 
-    public <T> Result<T, Exception> getField(Function<V, T> methode) {
-        if (value == null) {
-            return Result.reason(exception);
+    public <R> R map(Function<V,R> func,R r ){
+        if (this.state.equals(State.OK)){
+            return func.apply(this.ok);
         } else {
-            try {
-                T t = methode.apply(value);
-                if (t == null) {
-                    return Result.reason(new RuntimeException("Methode call returns null"));
-                }
-                return Result.ok(t);
-            } catch (Exception e) {
-                return Result.error(e);
-            }
+            return r;
         }
     }
 
-    public ResultStatus getStatus() {
-        return status;
+    public void match(Consumer<V> vConsumer, Consumer<E> eConsumer){
+        if (state.equals(State.OK)){
+            vConsumer.accept(ok);
+        } else {
+            eConsumer.accept(error);
+        }
     }
+
+    public V unwrap() throws IllegalStateException{
+        if (state.equals(State.OK)){
+            return ok;
+        }
+        throw new IllegalStateException("Try to unwrap an error Result");
+    }
+
+
+    private enum State{
+        OK,
+        ERROR;
+    }
+
+
 
 
 }
