31f0ef91c9fb3d3a13615fb6c6fc36486e256169
Tyrael <xykingofskillyx@gmail.com>
2024-01-11 00:45:25 +0100

    Remove Inventory

--- src/main/java/de/aethos/lib/inventory/item/FunctionalEventHandler.java
+++
@@ -1,9 +0,0 @@
-package de.aethos.lib.inventory.item;
-
-import org.bukkit.event.Event;
-
-@Deprecated
-@FunctionalInterface
-public interface FunctionalEventHandler<T extends Event> {
-    void onEvent(T t);
-}
